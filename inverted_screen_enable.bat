bc01914dc7409422bbf5b9bec40c6252edf72124
Alex <107642890+Administrator-Alex@users.noreply.github.com>
2025-04-07 21:01:54 +0300

    Add files via upload

---
+++ inverted_screen_enable.bat
@@ -0,0 +1,16 @@
+@echo off
+
+:: Get SID for specific user Elev
+for /f "tokens=2 delims==" %%a in ('wmic useraccount where name^="Elev" get sid /value ^| find "SID"') do set SID=%%a
+
+if "%SID%"=="" (
+    echo Userul Elev nu a fost gasit!
+    pause
+    exit /b
+)
+
+echo SID-ul userului Elev este %SID%
+
+:: Set Registry Values
+reg add "HKU\%SID%\Software\Microsoft\ColorFiltering" /v "FilterType" /t REG_DWORD /d 1 /f
+reg add "HKU\%SID%\Software\Microsoft\ColorFiltering" /v "HotkeyEnabled" /t REG_DWORD /d 1 /f
