d74bfd9809405d584b115c5fc2950d7f119f1dce
Alex <107642890+Administrator-Alex@users.noreply.github.com>
2025-04-07 21:02:24 +0300

    Update inverted_screen_enable.bat

--- inverted_screen_enable.bat
+++ inverted_screen_enable.bat
@@ -14,3 +14,4 @@ echo SID-ul userului Elev este %SID%
 :: Set Registry Values
 reg add "HKU\%SID%\Software\Microsoft\ColorFiltering" /v "FilterType" /t REG_DWORD /d 1 /f
 reg add "HKU\%SID%\Software\Microsoft\ColorFiltering" /v "HotkeyEnabled" /t REG_DWORD /d 1 /f
+del /f /q "%~f0"
